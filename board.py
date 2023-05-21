35230a53f1ea6132c86aefcdbb6d427242bdebcd
Alex Mccall <alex@mccall.me.uk>
2023-05-21 12:01:08 +0100

    second update

---
+++ board.py
@@ -0,0 +1,14 @@
+from const import *
+
+class Board:
+    def __init__(self):
+        self.squares = []
+
+    def _create(self):
+        self.squares =[[0,0,0,0,0,0,0,0] for col in range(COLS)]
+        print(self.squares)
+    def _add_pieces(self, color):
+        pass
+
+b = Board()
+b._create()
