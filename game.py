3ed8b533200aac759ef488577e9a9b95f9def915
Alex Mccall <alex@mccall.me.uk>
2023-08-23 20:50:37 -0400

    can now drag a piece

--- game.py
+++ game.py
@@ -2,13 +2,14 @@ import pygame
 
 from const import *
 from board import Board
+from dragger import Dragger
 
 class Game:
 
     def __init__(self):
 
         self.board =  Board()
-
+        self.dragger = Dragger()
     # Show methods
 
     def show_bg(self, surface):
@@ -27,7 +28,12 @@ class Game:
                 #piece?
                 if self.board.squares[row][col].has_piece():
                     piece = self.board.squares[row][col].piece
-                    img = pygame.image.load(piece.texture)
-                    img_center = col * SQSIZE + SQSIZE / 2, row * SQSIZE + SQSIZE / 2
-                    piece.texture_rect =  img.get_rect(center = img_center)
-                    surface.blit(img, piece.texture_rect)
+
+                    # all pieces except dragger piece
+
+                    if piece is not self.dragger.piece:
+                        piece.set_texture(size=80)
+                        img = pygame.image.load(piece.texture)
+                        img_center = col * SQSIZE + SQSIZE / 2, row * SQSIZE + SQSIZE / 2
+                        piece.texture_rect =  img.get_rect(center = img_center)
+                        surface.blit(img, piece.texture_rect)
